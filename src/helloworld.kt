7676d738ab67e1cb5d2153f6787bb7d5210f73b6
columbooster <kinglove0403@gmail.com>
2021-07-22 18:54:42 +0900

    Initial commit

---
+++ src/helloworld.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>){
+    println("Hello World")
+    println("Jhansi")
+    println("you are 25!")
+    println("no line break")
+    println("")
+    println("with line \n break")
+    print("Happy Birthday!\n")
+    print("Jhansi\n")
+    print("you are 25!")
+}
